279a1f56698768f828b8aa5de4d74d4d215384fc
Victor <victorac2324@gmail.com>
2024-04-30 20:51:55 -0400

    used atrributes and properties to do deliverables:

--- lib/dog.py
+++ lib/dog.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python3
 
-APPROVED_BREEDS = [
+# APPROVED_BREEDS = [
+#     "Mastiff",
+#     "Chihuahua",
+#     "Corgi",
+#     "Shar Pei",
+#     "Beagle",
+#     "French Bulldog",
+#     "Pug",
+#     "Pointer"
+# ]
+
+
+class Dog:
+
+    APPROVED_BREEDS = [
     "Mastiff",
     "Chihuahua",
     "Corgi",
@@ -11,5 +25,18 @@ APPROVED_BREEDS = [
     "Pointer"
 ]
 
-class Dog:
-    pass
+    def __init__(self, name: str = "dalvin", breed: str = "Corgi"):
+        self.set_name(name)
+        self.set_breed(breed)
+
+    def set_name(self, name):
+        if type(name) == str and 1 <= len(name) <= 25:
+            self.name = name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    def set_breed(self, breed):
+        if breed in  self.APPROVED_BREEDS:
+            self.breed = breed
+        else:
+            print("Breed must be in list of approved breeds.")
--- lib/person.py
+++ lib/person.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python3
 
-APPROVED_JOBS = [
+# APPROVED_JOBS = [
+#     "Admin",
+#     "Customer Service",
+#     "Human Resources",
+#     "ITC",
+#     "Production",
+#     "Legal",
+#     "Finance",
+#     "Sales",
+#     "General Management",
+#     "Research & Development",
+#     "Marketing",
+#     "Purchasing"
+# ]
+
+class Person:
+    APPROVED_JOBS = [
     "Admin",
     "Customer Service",
     "Human Resources",
@@ -15,5 +31,19 @@ APPROVED_JOBS = [
     "Purchasing"
 ]
 
-class Person:
-    pass
+
+    def __init__(self, name:str ="Person", job:str="default job"):
+        self.set_name(name)
+        self.set_job(job)
+    
+    def set_name(self, name):
+        if type(name) == str and 1 <= len(name) <= 25:
+            self.name = name.title()
+        else:
+            print( "Name must be string between 1 and 25 characters.")
+       
+    def set_job(self, job):
+        if job in self.APPROVED_JOBS:
+            self.job=job
+        else:
+            print ("Job must be in list of approved jobs.")
